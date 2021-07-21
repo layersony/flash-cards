dada2399df5dbb1e050bef1733b1ff6f24cd7999
annechege <annechege780@gmail.com>
2021-07-21 17:30:39 +0300

    url changing

--- cards/urls.py
+++ cards/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
   path('flashcards/', views.FlashCardsList.as_view()),
+  path('api/flashcards/' ,views.FlashCardsList.as_view()),
   # path('flashcards/<int:pk>', views.FlashCardsList.as_view()),
   # path('subjects/', views.SublectList.as_view()),
   # path('subjects/<int:pk>', views.SublectList.as_view()),
